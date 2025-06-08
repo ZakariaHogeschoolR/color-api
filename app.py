5d536873a7a13ddbda97f14785b615978a3b5e8b
ZakariaHogeschoolR <ztahiri26@gmail.com>
2025-06-08 12:25:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
 from train import generate_sample, train # your function that uses the model
 app = FastAPI()
@@ -12,7 +12,7 @@ class ColorOutput(BaseModel):
     colors: list[str]
 
 @app.api_route("/generate", methods=["GET", "POST"])
-def generate_palette(input: PromptInput):
+def generate_palette(request: Request, input: PromptInput):
     if request.method == "POST":
         try:
             result = train()
